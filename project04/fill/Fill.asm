56f5b350f22c2bf473aba526ed16f30acc27a227
Chris Moylan <chris@chrismoylan.com>
2012-09-09 02:47:59 -0500

    Fill.asm works

--- project04/fill/Fill.asm
+++ project04/fill/Fill.asm
@@ -13,36 +13,83 @@
 // otherwise unfill
 
 
+// define max screen (24576)
 @24576
 D=A
 @MXSCREEN
-M=D         // define max screen
+M=D
 
-// @16384
+
+// define screen pointer (16384)
 @SCREEN
 D=A
-@SCREENCOUNTER
-M=D         // define screen counter
+@POINTER
+M=D
 
 
 (LOOP)     // infinite loop
+  // If we have keyboard input, jump to FILL
   @KBD
   D=M
   @FILL
   D; JGT
 
+  // Otherwise jump to UNFILL
+  @UNFILL
+  0; JMP
 
+
+(UNFILL)
+  // Do nothing if POINTER == SCREEN
+  @SCREEN
+  D=A
+  @POINTER
+  D=D-M
+  @LOOP
+  D; JEQ
+
+  // Unfill the screen
+  D=0
+  @POINTER
+  A=M
+  M=D
+
+  // Decrement POINTER
+  D=1
+  @POINTER
+  D=M-D
+  @POINTER
+  M=D
+
+  // Jump back to main loop
   @LOOP
   0; JMP
 
+
 (FILL)
-  // starting at the counter
-  // fill in 1 pixel of the screen
-  @SCREENCOUNTER
-  M=1
-  D=M+1
+  // Do nothing if the screen is full
   @MXSCREEN
+  D=M
+  @POINTER
+  D=D-M
+  @LOOP
+  D; JEQ
+
+
+  // Fill in the pixel that POINTER points to
+  //@-1
+  D=-1
+  @POINTER
+  A=M
   M=D
 
+  // Iterate pointer by 1
+  D=1
+  @POINTER
+  D=D+M
+  @POINTER
+  M=D
+
+  // jump back to main loop
   @LOOP
-  0; JMP   // jump back to main loop
+  0; JMP
